97a3ba6dfc50f1ca160de914fef6da78bf8463bd
MicaelRadson <150195153+MicaelRadson@users.noreply.github.com>
2025-11-07 21:02:53 -0300

    RED: adicionar teste para depósito positivo

--- tests/test_conta_bancaria.py
+++ tests/test_conta_bancaria.py
@@ -3,3 +3,8 @@ from src.conta_bancaria import ContaBancaria
 def test_conta_comeca_com_saldo_zero():
     conta = ContaBancaria()
     assert conta.saldo == 0
+
+def test_deposito_aumenta_saldo():
+    conta = ContaBancaria()
+    conta.depositar(100)
+    assert conta.saldo == 100
