565de945b968019a3c1c425b83e64991f72ce83f
MicaelRadson <150195153+MicaelRadson@users.noreply.github.com>
2025-11-07 21:10:43 -0300

    RED: adicionar teste para saque válido

--- tests/test_conta_bancaria.py
+++ tests/test_conta_bancaria.py
@@ -8,3 +8,9 @@ def test_deposito_aumenta_saldo():
     conta = ContaBancaria()
     conta.depositar(100)
     assert conta.saldo == 100
+
+def test_saque_valido_diminui_saldo():
+    conta = ContaBancaria()
+    conta.depositar(200)
+    conta.sacar(50)
+    assert conta.saldo == 150
